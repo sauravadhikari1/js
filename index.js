f8f12de44f95dd3e385972ad64615ac6782c1879
sauravadhikari1 <sauravadhikari456@gmail.com>
2024-08-15 07:52:20 +1000

    fiurst commit

---
+++ index.js
@@ -0,0 +1,91 @@
+//Reverse a string
+
+/*var a= "Saurab"
+let rev='';
+for (mat of a){
+    rev=  mat+rev
+    // console.log(mat)
+}
+console.log(rev)*/
+
+
+
+//Sum of an array
+
+
+
+/*let arr=[2,3,4,5]
+let sum=0;
+for (each of arr){
+sum=sum+each
+}
+console.log(sum)*/
+
+
+// palindrome checker
+
+
+// var a= "racecar"
+// let rev='';
+// for (mat of a){
+//     rev=  mat+rev
+//     // console.log(mat)
+// }
+// if(a==rev){
+//     console.log("this is a palindromme string")
+// }
+
+// else{
+//     console.log("this is not a palindrome string")
+// }
+
+
+//print all the factors
+
+
+
+// let n = 96
+// for(i=1; i<=Math.sqrt(n);i++){
+//     if (n%i==0){
+
+//         if(n/i==i){
+//             console.log(i);
+
+//         }
+//         else
+//         console.log(i + " "+ (n/i)+ " ")
+
+//     }
+// }
+
+
+//largest number in an array
+
+
+// arr=[2,5,8,7,3,5,4,55]
+// let largest=arr[0]
+// for(i of arr){
+//     if (i>largest){
+//         largest=i
+//     }
+// }
+// console.log("largest number is ")
+// console.log(largest)
+
+
+
+// number of vowels in a string
+
+// let arr=['a','e','i','o','u']
+// word="hello world"
+// count=0
+// for(i of word){
+//     for(j of arr){
+//         if(i==j){
+//             count++;
+//         }
+//     }
+// }
+// console.log(count)
+
+
